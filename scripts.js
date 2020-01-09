e6498eda947e4edaba42965dd9401ca5e8a4933e
Chau <chaud@outlook.com>
2020-01-08 21:30:42 -0500

    added function to check if answer is right

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ var answerFour = questions[0].choices[3];
 var selectAnswer = questions[0].answer;
 var i = 0;
 var answerList = questions[0];
+var usrChoice;
 
 var answerBtn1 = document.querySelector("#answer1");
 var answerBtn2 = document.querySelector("#answer2");
@@ -21,22 +22,29 @@ document.getElementById("answer4").innerText = answerFour;
 
 answerBtn1.addEventListener("click", function(){
     event.preventDefault();
+    usrChoice = answerOne;
+    checkAnswer();
     setAnswer();
 
 });
 answerBtn2.addEventListener("click", function(){
     event.preventDefault();
+    usrChoice = answerTwo;
+    checkAnswer();
     setAnswer();
 
 });
 answerBtn3.addEventListener("click", function(){
     event.preventDefault();
-    i++;
+    usrChoice = answerThree;
+    checkAnswer();
     setAnswer();
 
 });
 answerBtn4.addEventListener("click", function(){
     event.preventDefault();
+    usrChoice = answerFour;
+    checkAnswer();
     setAnswer();
 });
 
@@ -47,10 +55,21 @@ function setAnswer(){
     answerTwo = questions[i].choices[1];
     answerThree = questions[i].choices[2];
     answerFour = questions[i].choices[3];
+    selectAnswer = questions[i].answer;
     console.log("infunction");
     document.getElementById("Questions").innerText = questions1;
     document.getElementById("answer1").innerText = answerOne;
     document.getElementById("answer2").innerText = answerTwo;
     document.getElementById("answer3").innerText = answerThree;
     document.getElementById("answer4").innerText = answerFour;
+}
+
+function checkAnswer(){
+    selectAnswer = questions[i].answer;
+    if (selectAnswer === usrChoice){
+        console.log("right");
+    }
+    else{
+        console.log("not right");
+    }
 }
