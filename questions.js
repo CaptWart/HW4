253c5334bcbaf5777304db78fe14b0fba1e474a1
Chau <chaud@outlook.com>
2020-01-08 21:16:24 -0500

    added logic for questions and answers

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "question 1",
+        choices: ["Answer1", "Answer2", "Answer3", "Answer4"],
+        answer: "Answer1"
+    },
+    {
+        title: "question 2",
+        choices: ["Answer1", "Answer2", "Answer3", "Answer4"],
+        answer: "Answer2"
+    },
+    {
+        title: "question 3",
+        choices: ["Answer1", "Answer2", "Answer3", "Answer4"],
+        answer: "Answer3"
+    },
+    {
+        title: "question 4",
+        choices: ["Answer1", "Answer2", "Answer3", "Answer4"],
+        answer: "Answer4"
+    },
+  ];
